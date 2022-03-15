5be84c8c7e17fdb9309bd924773836574f0a81c1
justinkim2015 <justinkim2015@gmail.com>
2022-03-15 13:02:56 +0900

    Finish up until test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    return array.splice(num, 1);
 };
+
+//so this num is the number that should be removed from the array.  
+//For example, if it's 3 then space 3 will be taken out.
+//use .foreach and .push
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    let answer = 0;
+
+    if (start <= finish) {
+        for (let i = start; i <= finish; i++) {
+          if (start < 0 || finish < 0) {
+              return 'ERROR';
+          } else {
+             answer += i;
+          }
+        }
+    return(answer)
+    } else {
+        for (let i = finish; i <= start; i++) {
+            if (start < 0 || finish <0) {
+                return 'ERROR';
+            } else {
+                answer += i;
+            }
+        }
+    return(answer)    
+    }
+
+
+
+
+
+
+
+
+
 
 };
 
+
+//npm test sumAll.spec.js
+
+
+//code wont run for test 3 because i is bigger than finish.
+//if i switch the two numbers then it wont work with
+//bigger numbers second...
+//
+
+
 // Do not edit below this line
 module.exports = sumAll;
+
