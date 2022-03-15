aac47801e1b3a4d018d9bf5efd03d677a2d24244
justinkim2015 <justinkim2015@gmail.com>
2022-03-15 22:11:01 +0900

    Finish exercise 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
 if ((year % 4 == 0) && (year % 400 == 0)) {
     return true
-} else if (year % 4 == 0){
+} else if (year % 4 == 0 && year % 100 !==0){
     return true
 } else {
     return false
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  return Math.round(((deg - 32) * (5 / 9))*10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  return Math.round((deg * (9/ 5) + 32) * 10) / 10;
 };
+//npm test tempConversion.spec.js
 
 // Do not edit below this line
 module.exports = {
