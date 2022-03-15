8fcc1b744457a9d6d5904537c4e8d4fef924db78
justinkim2015 <justinkim2015@gmail.com>
2022-03-15 13:42:57 +0900

    Finish tests 1 to 3

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+if ((year % 4 == 0) && (year % 400 == 0)) {
+    return true
+} else if (year % 4 == 0){
+    return true
+} else {
+    return false
+}
 };
+
+//npm test leapYears.spec.js
 
 // Do not edit below this line
 module.exports = leapYears;
